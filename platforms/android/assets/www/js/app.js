b132df5512bc4f9f49e77fd101d976cd7f1d2050
konpyu <konpyu@gmail.com>
2015-04-04 19:19:13 +0900

    implement

--- platforms/android/assets/www/js/app.js
+++ platforms/android/assets/www/js/app.js
@@ -1,56 +1,55 @@
 (function(){
   'use strict';
-  var module = angular.module('app', ['onsen']);
+  var module = angular.module('app', ['onsen', 'ngResource']);
 
-  module.controller('AppController', function($scope, $data) {
-    $scope.doSomething = function() {
-      setTimeout(function() {
-        alert('tapped');
-      }, 100);
-    };
+  //-------------------------------------------------------------------------------------------
+  // controller
+
+  module.controller('AppController', function($scope, Restaurant) {
+      $scope.doSomething = function() {
+          setTimeout(function() {
+              alert('tapped');
+          }, 100);
+      };
   });
 
-  module.controller('DetailController', function($scope, $data) {
-    $scope.item = $data.selectedItem;
+  module.controller('DetailController', function($scope, Restaurant) {
+      $scope.item = Restaurant.getSelectedItem();
   });
 
-  module.controller('MasterController', function($scope, $data) {
-    $scope.items = $data.items;  
-    
-    $scope.showDetail = function(index) {
-      var selectedItem = $data.items[index];
-      $data.selectedItem = selectedItem;
-      $scope.navi.pushPage('detail.html', {title : selectedItem.title});
-    };
+  module.controller('MasterController', function($scope, Restaurant) {
+      Restaurant.getData().then(function(data){
+          $scope.items = data.restaurants;
+      });
+
+      $scope.showDetail = function(index) {
+          var selectedItem = $scope.items[index];
+          Restaurant.setSelectedItem(selectedItem);
+          $scope.navi.pushPage('detail.html', {title : selectedItem.title});
+      };
   });
 
-  module.factory('$data', function() {
-      var data = {};
-      
-      data.items = [
-          { 
-              title: 'Item 1 Title',
-              label: '4h',
-              desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+  //-------------------------------------------------------------------------------------------
+  // api
+
+  module.factory('Restaurant', function($q, $timeout, $resource) {
+      var selectedItem = {};
+      return {
+          getData: function() {
+              var deferred = $q.defer();
+              $resource("https://tabearuki.herokuapp.com/restaurants.json").get(
+                  function(res){
+                    deferred.resolve(res);
+                  }
+              );
+              return deferred.promise;
           },
-          { 
-              title: 'Another Item Title',
-              label: '6h',
-              desc: 'Ut enim ad minim veniam.'
+          setSelectedItem: function(arg) {
+              selectedItem = arg;
           },
-          { 
-              title: 'Yet Another Item Title',
-              label: '1day ago',
-              desc: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'
-          },
-          { 
-              title: 'Yet Another Item Title',
-              label: '1day ago',
-              desc: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'
+          getSelectedItem: function() {
+              return selectedItem;
           }
-      ]; 
-      
-      return data;
+      };
   });
 })();
-
